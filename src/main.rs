7e8892fbf060ef079c3da7a4a808890de98dc731
vickyone0 <vigneshs0.618@gmail.com>
2025-05-30 00:33:30 -0400

    1000 request to url

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 use std::thread;
 
 fn main() {
+
     let mut handles = vec![];
 
-    for i in 0..100 {
+    for i in 0..1000 {
+
         let handle = thread::spawn(move || {
-            println!("Hello from thread {}", i);
-            let client = reqwest::blocking::Client::new();
-            let res = client
-                .post("http://httpbin.org/post")
-                .body("the exact body that is sent")
-                .send();
-            println!("Response from thread {}: {:?}", i, res);
+            let res = reqwest::blocking::Client::new()
+            .get("https://www.gecidukki.ac.in/")
+            .body("Hello, world!")
+            .send();
+            match res {
+                Ok(response) => {
+                    if response.status().is_success() {
+                        println!("Thread {}: Request successful", i);
+                    } else {
+                        println!("Thread {}: Request failed with status: {}", i, response.status());
+                    }
+                },
+                Err(e) => println!("Thread {}: Request error: {:?}", i, e),
+            }
         });
 
         handles.push(handle);
     }
 
     for handle in handles {
-        handle.join().unwrap();
+        match handle.join() {
+            Ok(_) => println!("Thread completed successfully"),
+            Err(e) => println!("Thread failed: {:?}", e),
+        }
     }
 }
